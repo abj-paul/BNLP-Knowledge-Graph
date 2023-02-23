accc1f7a44567ac15f56de85c3c8436721f7f102
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-02-23 23:34:47 +0600

    Public repository for the project

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+unzip paper-reviews.zip -d ~
+unzip emacs-configuration.zip -d ~
+sudo apt install emacs
+echo "Done installing and placing dependencies!"
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm ~/Org-Roam-Files -r
+rm ~/.emacs.d -rf
+sudo apt remove emacs
+echo "Done uninstalling and placing dependencies!"
